85c82072a04bbd993b43aa67db46d8cc89f5b09a
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-24 21:35:05 +0700

    a

--- batdong.js
+++ batdong.js
@@ -1,2 +1,28 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=Saigon'
+const request = require('request')
+
+function getDataCity(cityname ){
+    return new Promise((reject, resolve) => {
+        const URL = `http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityname}`
+
+        request(URL , {json : true}, function(error , response , body){
+            if(error) return reject(error.message)
+            if(body.message) return reject(body.message)
+            resolve(body.main.temp)
+        })
+    })
+}
+
+getDataCity('S')
+.then(data => console.log(data))
+.catch(error => console.log(error))
+
+function cong,tru,nhan,chia(a ,b){
+
+}
+function dientichHinhchunhat(params) {
+    
+}
+function chuvinhat(params) {
+    
+}
 
