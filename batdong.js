6032b6cf554ae34cc386d550bd070baaf703bdd9
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-24 21:00:27 +0700

    A

--- batdong.js
+++ batdong.js
@@ -1,12 +1,2 @@
-function getDataDelayOneSecond(fn){
-    let count = 10
-    setTimeout(()=> {
-        count++
-        fn(count)
-    },1000)
-}
-getDataDelayOneSecond(function(count){
-    console.log(count)    
-})
+const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=Saigon'
 
-http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=Saigon
