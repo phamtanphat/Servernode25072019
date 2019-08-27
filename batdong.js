4272f1a48b64519a79f2ecbb48500add63392503
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-27 19:53:18 +0700

    A

--- batdong.js
+++ batdong.js
@@ -1,28 +1,42 @@
 const request = require('request')
 
-function getDataCity(cityname ){
-    return new Promise((reject, resolve) => {
-        const URL = `http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityname}`
 
+https://pheptinhonline.herokuapp.com/chia/4/0
+function cong(a ,b){
+    return new Promise((reject , resolve) => {
+        const URL = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}`
         request(URL , {json : true}, function(error , response , body){
             if(error) return reject(error.message)
-            if(body.message) return reject(body.message)
-            resolve(body.main.temp)
+            if(typeof body.message != Number ) return reject(body.message)
+            resolve(body.message)
         })
     })
 }
-
-getDataCity('S')
-.then(data => console.log(data))
+cong(10 , 5)
+.then(value => console.log(value))
 .catch(error => console.log(error))
 
-function cong,tru,nhan,chia(a ,b){
-
-}
-function dientichHinhchunhat(params) {
+// function dientichHinhchunhat(params) {
     
-}
-function chuvinhat(params) {
+// }
+// function chuvinhat(params) {
     
-}
+// }
 
+
+
+// function getDataCity(cityname ){
+//     return new Promise((reject, resolve) => {
+//         const URL = `http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityname}`
+
+//         request(URL , {json : true}, function(error , response , body){
+//             if(error) return reject(error.message)
+//             if(body.message) return reject(body.message)
+//             resolve(body.main.temp)
+//         })
+//     })
+// }
+
+// getDataCity('S')
+// .then(data => console.log(data))
+// .catch(error => console.log(error))
