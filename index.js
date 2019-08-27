8e5b5041b182c6fec4f634966e24be35536e04c6
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-27 21:28:22 +0700

    a

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 const express = require("express")
-const {json} = require("body-parser")
 const app = express()
+const {json} =  require("body-parser")
+const {Word} = require("./word.model")
 
 app.use(json())
-
-app.get("/cong/:a/:b" , (req,res) => {
-    const {a , b} = req.params
-    if(isNaN(a) || isNaN(b)){
-        res.send({success : false , message : "Khong phai là số"})
-        return res.end()
-    }
-    const ketqua = +a + +b
-    res.send({success : true , tong : ketqua})
-
+// truong hoac round
+app.get("/word" , (req , res) => {
+    Word.find({})
+    .then(words => res.send({success : true , words}))
+    .catch(error => res.send({success : false , message : error}))
 })
-app.post("/chia" , (req,res) => {
-    const {a , b} = req.body
-    if(isNaN(a) || isNaN(b)){
-        res.send({success : false , message : "Khong phai là số"})
-        return res.end()
-    }
-    const ketqua = +a / +b
-    res.send({success : true , chia : ketqua})
 
+//insert 
+app.post("/word" , (req , res) => {
+    const {en , vn } = req.body
+    if(en.trim() === '' || vn.trim() === ''){
+        return res.send({success : false , message : "Emty value"})
+    }
+    const newword = new Word({en , vn})
+    newword.save()
+    .then(w => res.send({success : true , word : w}))
+    .catch(error => res.send({success : false , message : error}))
 })
-app.listen("3000" , () => console.log("Server started"))
+//update
+app.put("/word/:_id" , (req , res) => {
+    const {_id} = req.params
+    const {isMemorized} = req.body
+    if(_id.trim() === '' || isMemorized === null){
+        return res.send({success : false , message : "Emty value"})
+    }
+    Word.findByIdAndUpdate(_id,{isMemorized},{new : true})
+    .then(w => res.send({success : true , word : w}))
+    .catch(error => res.send({success : false , message : error}))
+})
+//delete
+app.delete("/word/:_id" , (req , res) => {
+    const {_id} = req.params
+    if(_id.trim() === ''){
+        return res.send({success : false , message : "Emty value"})
+    }
+    Word.findByIdAndDelete(_id)
+    .then(w => res.send({success : true , word : w}))
+    .catch(error => res.send({success : false , message : error}))
+})
+
+app.listen("3000",() => console.log("Server started"))
--- word.model.js
+++ word.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://abc_1:ABC123456@databasenode-fx0di.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true , useCreateIndex: true});
+mongoose.connect('mongodb+srv://abc:123@tuvung-smiqa.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true , useCreateIndex: true});
 
 const Word = mongoose.model('Word', { 
     en : {type : String , require : true , trim : true , unique : true},
@@ -13,17 +13,17 @@ module.exports = {Word}
 // .then(words => console.log(words))
 
 //INSERT 
-// const newword = new Word({en : "Five" , vn : "Năm"})
+// const newword = new Word({en : "Six" , vn : "Sáu"})
 // newword.save()
 // .then(w => console.log(w))
 // .catch(error => console.log(error.message))
 
 //UPDATE
-// Word.findByIdAndUpdate("5d15fca8c06d1b04571adb78",{isMemorized : false} , {new : true})
+// Word.findByIdAndUpdate("5d653d399858c2168b320754",{isMemorized : true} , {new : true})
 // .then(w => console.log(w))
 // .catch(error => console.log(error.message))
 
 //DELETE 
-// Word.findByIdAndDelete("5d15fca8c06d1b04571adb78")
+// Word.findByIdAndDelete("5d653d399858c2168b320754")
 // .then(w => console.log(w))
 // .catch(error => console.log(error.message))
